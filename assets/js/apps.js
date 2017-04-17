a66cec46cdfa207fce05c5169b0e497f5af0691f
IlseMagali <ilsemke@gmail.com>
2017-04-17 15:09:27 +0000

    v2 con lightbox 170417

--- assets/js/apps.js
+++ assets/js/apps.js
@@ -1,7 +1,9 @@
 var modalBox = document.getElementById("modalbox"); //modalbox
+var modalImg = document.getElementById("img01");
 var images = [];  //contenido del modalbox
 
-
+// esta funcion recorre el arreglo que se guarda en images, por cada imagen,
+// para a cada imagen agregarle el evento de click que provoque la ejecucion de la funcionshowImage
 function addClickToImage(){
   images = document.getElementsByClassName("modal-content");
   console.log(images);
@@ -10,16 +12,23 @@ function addClickToImage(){
   }
 }
 
+// mostrar la imagen al dar el click
 function showImage(){
  //alert("hola");
- images = document.getElementsByClassName("modal-content");
+ //el estilo del div modalBox se vuelve visible
  modalBox.style.display = "block";
- images[i].src = this.src;
+ // la src de la imagen se vuelve la src de this (objeto imagen) que debe mostrar o de la imagen que se le ha dado click
+ modalImg.src = this.src;
 }
 
-var span = document.getElementsByClassName("close")[0]; //boton de cerrar
-span.onclick = function() {
-  
+var span = []; //boton de cerrar
+
+for (var i = 0; i < span.length; i++){
+  span[i] = document.getElementsByClassName("close")[0];
+  span[i].addEventListener("click", close);
+}
+
+function close() {
   modalBox.style.display = "none";
 }
 
